745e49c60b5531dfaefedda4af97de8d6ea09571
Artem Beznosov <beznosov1410@gmail.com>
2026-06-20 00:41:48 +0300

    Removed unnecessary field in struct

--- include/ast/expression.hpp
+++ include/ast/expression.hpp
@@ -33,7 +33,6 @@ using expression = std::variant<
 struct literal_expr {
     core::token value_{};
     core::location loc_{};
-    mutable std::optional<core::value> cached_{};
 
     literal_expr(const core::token& value, core::location loc)
         : value_(value), loc_(loc) {}
